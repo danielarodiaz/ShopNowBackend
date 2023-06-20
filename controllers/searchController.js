332354984948bef027c0a54b228d77fa8f6682e0
dani-stack22 <danirocioinf@gmail.com>
2023-06-20 15:10:38 -0300

    Buscador de Usuarios, Categorias y Productos chequeados

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -2,7 +2,7 @@ const { request, response } = require("express");
 
 const Users = require("../models/UserModel");
 const Categories = require("../models/categoryModel");
-//const Products = require("../models/ProductModel")
+const Products = require("../models/ProductsModels");
 
 const collectionPermit = ["users", "categories", "products"];
 
@@ -26,16 +26,16 @@ const searchCategories = async (term, res = response) => {
     results: category,
   });
 };
-// const searchProducts = async (term, res = response) => {
-//   const regex = new RegExp(term, "i");
-//   const product = await Products.find({
-//     $or: [{ name: regex }, { description: regex }],
-//     $and: [{ status: true }],
-//   });
-//   res.json({
-//     results: product,
-//   });
-// };
+const searchProducts = async (term, res = response) => {
+  const regex = new RegExp(term, "i");
+  const product = await Products.find({
+    $or: [{ name: regex }, { description: regex }],
+    $and: [{ status: true }],
+  });
+  res.json({
+    results: product,
+  });
+};
 
 const search = (req = request, res = response) => {
   const { collection, term } = req.params;
--- models/Server.js
+++ models/Server.js
@@ -10,8 +10,8 @@ class Server {
     this.authPath = "/api/auth";
     this.userPath = "/api/users";
     this.categoryPath = "/api/categories";
-    this.searchPath = "/api/search";
     this.productPath = "/api/products";
+    this.searchPath = "/api/search";
     this.conectarDB();
 
     this.middlewars();
@@ -32,8 +32,8 @@ class Server {
     this.app.use(this.authPath, require("../routes/authRouter"));
     this.app.use(this.userPath, require("../routes/userRouter"));
     this.app.use(this.categoryPath, require("../routes/categoryRouter"));
-    this.app.use(this.searchPath, require("../routes/searchRouter"));
     this.app.use(this.productPath, require("../routes/productRouter"));
+    this.app.use(this.searchPath, require("../routes/searchRouter"));
   }
 
   listen() {
